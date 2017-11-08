34fda92737a3b9948383972e0043e639e842a1aa
fabiogm <fmagalhaes@gmail.com>
2017-11-08 11:10:43 -0200

    Add missing file containing function for window extraction.

---
+++ extract_window.m
@@ -0,0 +1,17 @@
+function [R] = extract_window(matrix, window, invert_selection)
+   rows = window.row:(window.row + window.height - 1);
+   cols = window.col:(window.col + window.width - 1);
+   
+   if nargin < 3
+       invert_selection = false;
+   end
+   
+   if invert_selection
+       tmpMatrix = matrix;
+       tmpMatrix(rows, cols) = nan;
+       isNan = isnan(tmpMatrix);
+       R = tmpMatrix(~isNan);
+   else
+       R = matrix(rows, cols);
+   end
+end
