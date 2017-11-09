cb6ba0d23808caa32095cea734c5a61d6e33f315
fabiogm <fmagalhaes@gmail.com>
2017-11-09 09:17:45 -0200

    Add ROC.

--- metrics.m
+++ metrics.m
@@ -1,4 +1,4 @@
-function [pd, pfa] = metrics(likelihood_matrix, labels, threshold)
+function [pd, pfa] = metrics(likelihoodMatrix, labels, threshold)
     % Calculates probability of detection and probability of false alarm
     % based on a matrix of likelihoods for each pixel of images (that is,
     % the matrix has format (512, 512, number of images), a vector
@@ -10,7 +10,7 @@ function [pd, pfa] = metrics(likelihood_matrix, labels, threshold)
     % PFA = NFA / (Nimg - NB) (Nimg - number of pixels in the considered
     %   sample, NB - number of pixels of real targets, NFA - number of 
     %   pixels falsely detected as targets).
-    aboveThreshold = likelihood_matrix > threshold;
+    aboveThreshold = likelihoodMatrix > threshold;
     s = sum(sum(aboveThreshold, 1));
     aboveThreshold = s ~= 0;
     aboveThreshold = aboveThreshold(:);
@@ -22,11 +22,13 @@ function [pd, pfa] = metrics(likelihood_matrix, labels, threshold)
     pd = ND/NP;
     
     % All images assumed to be 512*512 pixels.
-    %imgSize = 512 * 512;
-    %Nimg = imgSize * length(complete_matrix(:,1));
-    %NB = NP * imgSize;
-    %NFA = abs(ND-NP) * imgSize;
-    %pfa = NFA / (Nimg - NB);
-    pfa = 0;
+    imgSize = 512 * 512;
+    s = size(likelihoodMatrix);
+    Nimg = imgSize * s(3);
+    % 5*5 targets
+    NB = NP * 5 * 5;
+    % TODO. Ideally we should only consider 5x5 connected componets that
+    % have pixels in the center with higher likelihood, and not all pixels.
+    NFA = sum(likelihoodMatrix(:) > threshold);
+    pfa = NFA / (Nimg - NB);
 end
-
